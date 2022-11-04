b4bd0b7727e06399b12eeca5d167ae41c101f23f
priscila.teodoro <priteodoro01@gmail.com>
2022-11-03 22:11:27 -0400

    Verificar result

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,26 +39,62 @@ const form = document.querySelector('#form') //acessar a partir do id
 
 const dataTarefa = document.querySelector('#data')
 
-const listaTarefas = []
+let listaTarefas = []
 
 function limparFormulario() {
     descricaoTarefa.value = ''
     dataTarefa.value = ''
 }
 
-form.addEventListener('submit', function(event){ // escuta ação (passar o mouse, clicar), no caso do formulário é submeter. A função tem como parâmetro o evento
+const tarefasToLocalStorage = () => { //constante que aponta para uma função anônima (sem nome)
+    localStorage.setItem("tarefas", JSON.stringify(listaTarefas))
+}
+
+const getTarefasFromLocalStorage = () => {
+    listaTarefas = JSON.parse(localStorage.getItem("tarefas"))
+}
+
+const result = document.querySelector('#result')
+
+const setResultToTable = () => { //mostrar os resultados (tarefa) na tabela 
+    
+    result.innerHTML = ''
+
+    listaTarefas.forEach(tarefa => { // método que percorre cada item do array. ForEach recebe uma fução como parâmetro
+        const linha = document.createElement('tr')
+
+        const colunaDescricao = document.createElement('td')
+        colunaDescricao.innerHTML = tarefa.descricao
+
+        const colunaData = document.createElement('td')
+        colunaData.innerHTML = tarefa.data
+        
+        linha.appendChild(colunaDescricao)
+        linha.appendChild(colunaData)
+        result.appendChild(linha)
+    }) 
+}
+
+form.addEventListener('submit', (event) => { // escuta ação (passar o mouse, clicar), no caso do formulário é submeter. A função tem como parâmetro o evento  //função anônima
     event.preventDefault() //não recarrega a página ao clicar em adicionar (submeter)
 
 
 const tarefa = { //objeto
     descricao: descricaoTarefa.value, 
-    data: dataTarefa.value,
+    data: new Date (dataTarefa.value),
     realizado: false
 }
 
 listaTarefas.push(tarefa)
 limparFormulario()
+tarefasToLocalStorage()
+getTarefasFromLocalStorage()
+setResultToTable()
 })
+
+getTarefasFromLocalStorage()
+setResultToTable()
+
 /* const botaoImprimir = document.querySelector('#imprimir')
 botaoImprimir.addEventListener('click', function (){ //hover, ao soltar o click, etc
     console.log('Clicou no botão!')
